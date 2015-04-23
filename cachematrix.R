c5902963f56b913bbc4fcee75bc946fb62294809
cristelle <cristelle@MacBook-Air-de-Cristelle.local>
2015-04-23 15:10:13 +0200

    programming assignment 2
    computes the inverse of a matrix using the caching properties of R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## calcultes the inverse of a matrix using caching properties
+## if the inverse has never been computed, it solves it
+## and puts it in a cache via setinverse() 
+## if it has already been computed, it gets it from the cache
+## via the setinverse() function
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x = matrix(c(1,2,3,4), nrow=2, ncol=2)) {
+  xinv <- NULL
+  set <- function(y) { # assigns the matrix to new value and initializes the inverse to NULL
+    x <<- y # deep assignment arrow
+    xinv <<- NULL
+  }
+    
+  get <- function() x # returns the current value of the matrix
+  setinverse <- function(inv) xinv <<- inv # simply sets the inverse to given 'inv' matrix
+  getinverse <- function() xinv # returns the current inverse, these 2 are similar to the set and get 
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) # makes the list of available functions 
+  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (amatrix) { # Computes, caches, and returns matrix inverse
+  xinv <- amatrix$getinverse() # to check if it is already cached
+  if(!is.null(xinv)) { # if so, it returns the cached value
+    message("getting cached data")
+    return(xinv)
+  }  # if it was not cached the inverse, it computes using the solve function
+     # this is actually the only place where the computation of the inverse is done  
+  data <- amatrix$get()
+  xinv <- solve(data)
+  amatrix$setinverse(xinv) # and once it has been calculated, it sets the inverse value in the cache
+  xinv # and returns the result
 }
+
+# things to remember : 
+# <-, always creates a variable in the current environment. 
+# The deep assignment arrow, <<-, never creates a variable in the current environment, 
+# but instead modifies an existing variable found by walking up the parent environments.
